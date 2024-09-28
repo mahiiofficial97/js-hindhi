621925a50cfb8ceca7729ed1b32c14a78ed5a9f4
mahiiofficial97 <131246895+mahiiofficial97@users.noreply.github.com>
2024-09-28 16:28:58 +0000

    new commit 2nd

---
+++ 01basics/01_vaibales.js
@@ -0,0 +1,10 @@
+const account_Id=144553
+let accuntemail="mahii@gmail.com"
+var accountPassword="1234"
+accountcity="jaypur"
+ 
+
+console.log(account_Id);
+console.table([accuntemail,account_Id,accountcity])
+
+
---
+++ 01basics/02datatypes.js
@@ -0,0 +1,9 @@
+"use strict"//treat all js code as newer version
+//alert(3+3) we are using alert not browser 
+// Data types=number ,bigint,string,boolean,null,undefiend,symbol
+
+//object
+let name="mahesh"
+console.log(typeof name)//op  string javascript main string small madhe ast 
+console.log(typeof null)//object
+console.log(typeof undefined)//undefiend
