f99a139e2e122f4905c2884a6b83ed40dc38520d
mahiiofficial97 <131246895+mahiiofficial97@users.noreply.github.com>
2024-09-28 17:53:02 +0000

    conversation in java script

---
+++ 01basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let val="3"
+let newvalue=Number(val)
+console.log(newvalue)
+
+let numbers=123
+let newstring=String(numbers)
+console.log(typeof newstring)
+
+//1=>true; 0=>false
+//"" =>false
+//"mahesh"=>true
