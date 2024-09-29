07c02b8d23a5ed2ef37664643afb247b03652506
mahiiofficial97 <131246895+mahiiofficial97@users.noreply.github.com>
2024-09-29 09:48:52 +0000

    dates function in  java scripts

---
+++ 01basics/dateinjavascript.js
@@ -0,0 +1,16 @@
+let mydate=new Date()
+console.log(mydate.toString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toDateString());
+console.log( typeof mydate );
+
+let mycreatedate=new Date("2023-01-14")
+console.log(mycreatedate);
+
+let mystamotstap=Date.now()
+console.log(mystamotstap);
+
+
+
+
+
