2cb43b4435b09478f140ba5fa2d631770f231fe5
mahiiofficial97 <131246895+mahiiofficial97@users.noreply.github.com>
2024-09-30 08:44:03 +0000

    javascript apis

---
+++ 02basics/05_objectApi.js
@@ -0,0 +1,44 @@
+// javascript of objects Api or destructuring 
+
+
+const course={
+    cousrsename:"jsinhindhi",
+    price:999,
+    courseinstructor:"mahesh hande"
+
+}
+
+course.instructor
+const {courseinstructor }=course
+console.log(courseinstructor);   //called this functinality is object destructur
+
+//react method
+
+// const navbar=(company)=>
+// {
+
+// }
+// navbar(company="hitesh")
+
+//json is loooking like json but key and value both will be in string formante below
+
+// {
+//     "name":"mahesh",
+//     "id":3477,
+//     "newname":"mahii"
+// }
+
+
+//some time api we get in arry formate
+
+[
+    {
+        id:1
+    },
+    {
+        
+    }
+]
+
+//for api fetching website  https://randomuser.me/ 
+//for understating the api tool is jsonformatter.  https://jsonformatter.org/
